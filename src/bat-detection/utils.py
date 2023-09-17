156e45c8d7b5abcb4949294d4f10a92fbf5f09f2
Sathish Kumar <sathishmahi433@gmail.com>
2023-09-17 19:30:11 +0530

    Update utils.py

--- src/bat-detection/utils.py
+++ src/bat-detection/utils.py
@@ -1,8 +1,24 @@
 import os
-import os
 import yaml
 
-def read_config(yaml_file_path:str = "config/config.yaml" )->dict:
+def read_config(yaml_file_path: str = "config/config.yaml") -> dict:
+    """
+    Read a YAML configuration file and return its contents as a dictionary.
+
+    Args:
+        yaml_file_path (str, optional): The path to the YAML configuration file.
+            Defaults to "config/config.yaml".
+
+    Returns:
+        dict: A dictionary containing the configuration data.
+
+    Raises:
+        FileNotFoundError: If the specified YAML file does not exist.
+        Exception: If there is an error while loading the YAML file.
+
+    Example:
+        >>> config_data = read_config("my_config.yaml")
+    """
     if not os.path.exists(yaml_file_path):
         raise FileNotFoundError(f"{yaml_file_path} not found")
     try:
